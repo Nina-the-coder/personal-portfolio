963f43eca732cced1ccceb7fefabbc6a85c3aa4b
Deepanshu <n09456791@gmail.com>
2025-02-15 00:09:42 +0530

    day2: made a very good basic layout and the template just need to fill in the data

--- personal-portfolio/script.js
+++ personal-portfolio/script.js
@@ -1,1 +1,27 @@
-console.log("hello");
+document.querySelectorAll('li a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault(); 
+        const sectionId = link.getAttribute('href'); 
+        const section = document.querySelector(sectionId); 
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+document.querySelectorAll('.navbar ul li').forEach((link) => {
+    link.addEventListener("click", () => {
+        const anchor = link.querySelector('a');
+        anchor.click();
+    });
+})
+
+const ctaBtn = document.getElementById("cta-btn");
+
+ctaBtn.addEventListener("click", () => {
+    const contactSection = document.querySelector('#contact'); 
+    contactSection.scrollIntoView({
+        behavior: 'smooth', 
+        block: 'start'      
+    });
+})
